d571b42502a8f7a18715972be9340d8013f02a7c
pedro <hdlguy@hotmail.com>
2017-10-03 13:56:37 -0600

    running with wisdom

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 
 int main()
 {
-    const int N = 65536;
+    const int N = 96*1024;
 
     fftw_complex *in, *out;
     fftw_plan p;
     //...
     in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
     out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+
+    fftw_import_wisdom_from_filename("./wisdomf");
     p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
     //...
     for (int i=0; i<1000; i++){
